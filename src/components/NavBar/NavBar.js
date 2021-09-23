0344514bb7b58a65204e7f2466651e94060472b5
AleEspinozama <espinozalezama@gmail.com>
2021-09-23 18:51:18 -0500

    re routing2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,13 +7,13 @@ export default function NavBar() {
     return (
         <header className="navbar">
            
-               <Link exact to="/" className= "title">Movie App</Link>
+               <Link exact to="/movie-app/" className= "title">Movie App</Link>
         
             <nav>
                 <ul className="list">
                     <li className="list-item">
-                        <NavLink exact to="/" >Home</NavLink>
-                        <NavLink to="/favs" >Favoritas</NavLink>
+                        <NavLink exact to="/movie-app/" >Home</NavLink>
+                        <NavLink to="/movie-app/favs" >Favoritas</NavLink>
                     </li>
                 </ul>
             </nav>
